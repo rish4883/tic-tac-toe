2ffafb0e58f91a8e3681cdbce268c6e89efc9a8e
Rishabh <rishabh4883@gmail.com>
2023-04-04 22:46:49 +0530

    Create game module to play the game

--- script.js
+++ script.js
@@ -1,36 +1,41 @@
 // Game board module which tracks the marks on the board and updates it when needed
 const gameBoard = (function() {
     let board = []
-    board = [];
     const maxSize = 9;
-   function updateBoard(mark, pos) {
-        if(pos < this.maxSize)
-            this.board[pos] = mark;
+    
+
+    function updateBoard(mark, pos) {
+        if(pos < maxSize)
+            board[pos] = mark;
     }
+
+    // Function takes in an index and returns true if any element is present in index
     function checkIfOccupied(pos) {
-        return this.board[pos] != null;
+        return board[pos] != null;
     }
+
+    const getMarkOnBoard = (pos) => board[pos];
+    
 
     return  {
         updateBoard,
-        checkIfOccupied
+        checkIfOccupied,
+        getMarkOnBoard
     }
 })();
 
+
 // Module to control the display on webpage. 
 const displayController = (function() {
-    let mark = '';
     let squares;
 
     function init() {
         squares = document.querySelectorAll('.square');
         squares.forEach((square) => {
             square.addEventListener('click', (e) => {
-                // if(gameBoard.checkIfOccupied) {return;}
-                // updateMark();
-                // e.target.textContent = this.mark;
-                // let index = e.target.id;
-                // gameBoard.updateBoard(this.mark, index);
+                let index = e.target.id;
+                let mark = game.getMarkToDisplay(index);
+                e.target.textContent = mark;
             });
         });
     }
@@ -51,16 +56,58 @@ const displayController = (function() {
 // Factory function to create player objects 
 const player = function(number, mark) {
     let score;
-    function setScore() {
-        this.score++;
-    }
+    const setScore = () => {score++};
 
-    function getScore() {
-        return this.score;
-    }
+    const getScore = () => score;
 
     return {
         setScore,
         getScore
     }
 }
+
+const game = (function() {
+        
+    let player1;
+    let player2;
+    let move;
+    let win;
+
+    function startGame() {
+        move = 0;
+        player1 = player(1, 'X');
+        player2 = player(2, 'O');
+        displayController.init();
+    }
+
+    function getMarkToDisplay(index) {
+        if(gameBoard.checkIfOccupied(index-1))
+            return gameBoard.getMarkOnBoard(index-1)
+        else {
+            move++;
+
+            let mark = move % 2 != 0 ? 'X' : 'O';
+            gameBoard.updateBoard(mark, index-1)
+            trackGameStatus()
+            return mark
+            
+        }
+    }
+
+    function trackGameStatus() {
+        if(move>=9) {
+            win = 'tie';
+        }
+
+
+    }
+    
+    return {
+        startGame,
+        getMarkToDisplay
+    }
+
+
+})();
+
+game.startGame();
