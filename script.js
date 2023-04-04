e46c4a0721e57b1a2368f04c86e1217ac1f26153
Rishabh <rishabh4883@gmail.com>
2023-04-04 13:46:09 +0530

    Add some descriptive comments

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Game board module which tracks the marks on the board and updates it when needed
 const gameBoard = (function() {
     let board = []
     board = [];
@@ -16,6 +17,7 @@ const gameBoard = (function() {
     }
 })();
 
+// Module to control the display on webpage. 
 const displayController = (function() {
     let mark = '';
     let squares;
@@ -46,6 +48,7 @@ const displayController = (function() {
 
 })();
 
+// Factory function to create player objects 
 const player = function(number, mark) {
     let score;
     function setScore() {
